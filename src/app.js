351644c78a9a1be44cf9ff9d1ce4faf7b3d18ed2
Kiwi254 <130325869+Kiwi254@users.noreply.github.com>
2023-05-31 08:54:21 -0700

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -34,9 +34,20 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
 let apiKey = "667d9f573c8af4c33457be5d561a9148";
-let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-console.log(apiUrl);
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+    console.log(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
